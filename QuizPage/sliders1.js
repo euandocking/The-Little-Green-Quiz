94033f0f5115d7337c700abb4b33ed3e79cf7d59
lubots <lubots@users.noreply.github.com>
2019-12-03 22:40:47 +0000

    Add js for the sliders

---
+++ QuizPage/sliders1.js
@@ -0,0 +1,113 @@
+////////////////////////////////////////////////////////////////////////////////
+var slider1 = document.getElementById("bagsThrown");
+var output1 = document.getElementById("print-bagsThrown");
+output1.innerHTML = slider1.value; // Display the default slider value
+// Update the current slider value (each time you drag the slider handle)
+slider1.oninput = function() {
+  output1.innerHTML = this.value;
+}
+/////////////////////////next slider////////////////////////////////////////////
+var slider2 = document.getElementById("bagsRecycled");
+var output2 = document.getElementById("print-bagsRecycled");
+output2.innerHTML = slider2.value; // Display the default slider value
+// Update the current slider value (each time you drag the slider handle)
+slider2.oninput = function() {
+  output2.innerHTML = this.value;
+}
+/////////////////////////next slider////////////////////////////////////////////
+var slider3 = document.getElementById("cupsThrown");
+var output3 = document.getElementById("print-cupsThrown");
+output3.innerHTML = slider3.value; // Display the default slider value
+// Update the current slider value (each time you drag the slider handle)
+slider3.oninput = function() {
+  output3.innerHTML = this.value;
+}
+/////////////////////////next slider////////////////////////////////////////////
+var slider4 = document.getElementById("cupsRecycled");
+var output4 = document.getElementById("print-cupsRecycled");
+output4.innerHTML = slider4.value; // Display the default slider value
+// Update the current slider value (each time you drag the slider handle)
+slider4.oninput = function() {
+  output4.innerHTML = this.value;
+}
+/////////////////////////next slider////////////////////////////////////////////
+var slider5 = document.getElementById("strawsThrown");
+var output5 = document.getElementById("print-strawsThrown");
+output5.innerHTML = slider5.value; // Display the default slider value
+// Update the current slider value (each time you drag the slider handle)
+slider5.oninput = function() {
+  output5.innerHTML = this.value;
+}
+/////////////////////////next slider////////////////////////////////////////////
+var slider6 = document.getElementById("strawsRecycled");
+var output6 = document.getElementById("print-strawsRecycled");
+output6.innerHTML = slider6.value; // Display the default slider value
+// Update the current slider value (each time you drag the slider handle)
+slider6.oninput = function() {
+  output6.innerHTML = this.value;
+}
+/////////////////////////next slider////////////////////////////////////////////
+var slider7 = document.getElementById("plBottlesThrown");
+var output7 = document.getElementById("print-plBottlesThrown");
+output7.innerHTML = slider7.value; // Display the default slider value
+// Update the current slider value (each time you drag the slider handle)
+slider7.oninput = function() {
+  output7.innerHTML = this.value;
+}
+/////////////////////////next slider////////////////////////////////////////////
+var slider8 = document.getElementById("plBottlesRecycled");
+var output8 = document.getElementById("print-plBottlesRecycled");
+output8.innerHTML = slider8.value; // Display the default slider value
+// Update the current slider value (each time you drag the slider handle)
+slider8.oninput = function() {
+  output8.innerHTML = this.value;
+}
+/////////////////////////next slider////////////////////////////////////////////
+var slider9 = document.getElementById("containersThrown");
+var output9 = document.getElementById("print-containersThrown");
+output9.innerHTML = slider9.value; // Display the default slider value
+// Update the current slider value (each time you drag the slider handle)
+slider9.oninput = function() {
+  output9.innerHTML = this.value;
+}
+/////////////////////////next slider////////////////////////////////////////////
+var slider10 = document.getElementById("containersRecycled");
+var output10 = document.getElementById("print-containersRecycled");
+output10.innerHTML = slider10.value; // Display the default slider value
+// Update the current slider value (each time you drag the slider handle)
+slider10.oninput = function() {
+  output10.innerHTML = this.value;
+}
+/////////////////////////next slider////////////////////////////////////////////
+var slider11 = document.getElementById("cutleryThrown");
+var output11 = document.getElementById("print-cutleryThrown");
+output11.innerHTML = slider11.value; // Display the default slider value
+// Update the current slider value (each time you drag the slider handle)
+slider11.oninput = function() {
+  output11.innerHTML = this.value;
+}
+/////////////////////////next slider////////////////////////////////////////////
+var slider12 = document.getElementById("cutleryRecycled");
+var output12 = document.getElementById("print-cutleryRecycled");
+output12.innerHTML = slider12.value; // Display the default slider value
+// Update the current slider value (each time you drag the slider handle)
+slider12.oninput = function() {
+  output12.innerHTML = this.value;
+}
+/////////////////////////next slider////////////////////////////////////////////
+var slider13 = document.getElementById("plasticOtherThrown");
+var output13 = document.getElementById("print-plasticOtherThrown");
+output13.innerHTML = slider13.value; // Display the default slider value
+// Update the current slider value (each time you drag the slider handle)
+slider13.oninput = function() {
+  output13.innerHTML = this.value;
+}
+/////////////////////////next slider////////////////////////////////////////////
+var slider14 = document.getElementById("plasticOtherRecycled");
+var output14 = document.getElementById("print-plasticOtherRecycled");
+output14.innerHTML = slider14.value; // Display the default slider value
+// Update the current slider value (each time you drag the slider handle)
+slider14.oninput = function() {
+  output14.innerHTML = this.value;
+}
+////////////////////////////////////////////////////////////////////////////////
